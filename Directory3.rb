607507503707847fb3465dc94dd47cb213c5bd11
Guy Calloway <[guyfcalloway@gmail.com]>
2019-07-18 11:40:24 +0100

    Loading students functionality added

--- Directory3.rb
+++ Directory3.rb
@@ -149,7 +149,7 @@ def print_footer
 end
 
 def print_menu
-  puts "\n1. Input the students\n2. Show the students\n3. Save the list to students.csv\n9. Exit"
+  puts "\n1. Input the students\n2. Show the students\n3. Save the list to students.csv\n4. Load list from students.csv\n9. Exit"
 end
 
 def show_students
@@ -165,7 +165,9 @@ def process(selection)
   when "2"
     show_students
   when "3"
-    saving_file
+    save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
@@ -181,12 +183,21 @@ def interactive_menu
   end
 end
 
-def saving_file
+def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
